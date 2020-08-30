c9c7ce9bbb5a0a4613e17956c932a68f5715148d
Laura Broder <laura.kokielov@gmail.com>
2020-08-30 23:30:17 +0300

    first commit

---
+++ tabs.js
@@ -0,0 +1,11 @@
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+  console.log(event.currentTarget);
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener("click", handleTabClick),
+);
