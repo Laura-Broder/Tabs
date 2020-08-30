b3354969c0aa9955d60f8245934eac15f6307423
Laura Broder <laura.kokielov@gmail.com>
2020-08-30 23:50:43 +0300

    update

--- tabs.js
+++ tabs.js
@@ -3,7 +3,17 @@ const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
 
 function handleTabClick(event) {
-  console.log(event.currentTarget);
+  tabPanels.forEach((panel) => (panel.hidden = true));
+
+  tabButtons.forEach((tab) => tab.setAttribute("aria-selected", false));
+
+  event.currentTarget.setAttribute("aria-selected", true);
+
+  const id = event.currentTarget.id;
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  tabPanel.hidden = false;
+
+  // method 2: turn tabPanels from nodelist to array and use .find().
 }
 
 tabButtons.forEach((button) =>
